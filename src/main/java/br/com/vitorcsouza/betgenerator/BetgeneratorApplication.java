5506c02f0ca461d71f3931a4c0766a155ee72dfa
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-04-23 20:25:13 -0300

    Inicio do projeto gerador de numeros para aposta em loteria

---
+++ src/main/java/br/com/vitorcsouza/betgenerator/BetgeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitorcsouza.betgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitorcsouza/betgenerator/BetgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitorcsouza.betgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
